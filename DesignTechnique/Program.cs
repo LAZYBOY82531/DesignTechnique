710a8e69b51c165b40a43184ccbe2187e0fbb0c0
HyeoukJin Kwon <fallere82531@gmail.com>
2023-04-27 19:21:43 +0900

    feat: Create new Problem project

--- DesignTechnique/Program.cs
+++ DesignTechnique/Program.cs
@@ -9,9 +9,40 @@
 		 * 문제를 풀 때 어떤 알고리즘 설계 기법을 쓰는지에 따라 효율성이 막대하게 차이
 		 * 문제의 성질과 조건에 따라 알맞은 알고리즘 설계기법을 선택하여 사용
 		 ******************************************************/
+
+        //하노이 탑 문제
+        public static void Move(int count, int start, int end)
+        {
+            if(count == 1)
+            {
+                int node = stick[start].Pop();
+                stick[end].Push(node);
+                Console.WriteLine($"{start} 스틱에서 {end}으로 {node} 이동");
+                return;
+            }
+            int other =(3 - start - end);
+
+            Move(count - 1, start, other);
+            Move(1, start, end);
+            Move(count - 1, other, end);
+        }
+        public static Stack<int>[] stick;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine(Recursion.Factorial(5));
+            Console.WriteLine(Recursion.Factorial(5) + "\n");
+
+            int nodeCount = 4;
+            stick = new Stack<int>[3];
+            for (int i = 0; i < stick.Length; i++)
+            {
+                stick[i] = new Stack<int>();
+            }
+            for(int i = nodeCount;i > 0; i--)
+            {
+                stick[0].Push(i);
+            }
+            Move(nodeCount, 0, 2);
         }
     }
 }
